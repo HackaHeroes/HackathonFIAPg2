0db7d028764af9a6ca94dcf69b3a4e4612633ab5
Paulo Eduardo Scatena <paulo.scatena@gmail.com>
2024-03-16 11:11:07 -0300

    Suporte ao FFMPEG

---
+++ Hackaton.Fiap.Grupo02.CrossCutting.FFMpegSupport/MpegSupport.cs
@@ -0,0 +1,18 @@
+﻿using FFMpegCore;
+using System.Drawing;
+
+namespace Hackaton.Fiap.Grupo02.CrossCutting.FFMpegSupport
+{
+    public class MpegSupport
+    {
+        public async Task<IMediaAnalysis> AnalisarVideo(string path)
+        {
+            return await FFProbe.AnalyseAsync(path);
+        }
+
+        public bool Snapshot(TimeSpan currentTime, string input, string output)
+        {
+            return FFMpeg.Snapshot(input, output, new Size(1920, 1080), currentTime);
+        }
+    }
+}
