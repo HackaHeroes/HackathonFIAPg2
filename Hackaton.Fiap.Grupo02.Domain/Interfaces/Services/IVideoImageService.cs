cce78e84039e7b512b0bc5bc617bd364d9701d6b
murakamironaldo <murakami@odoias.com.br>
2024-03-16 12:12:18 -0300

    Desenvolvimento da camada de services

---
+++ Hackaton.Fiap.Grupo02.Domain/Interfaces/Services/IVideoImageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton.Fiap.Grupo02.Domain.Interfaces.Services
+{
+    public class IVideoImageService
+    {
+    }
+}
