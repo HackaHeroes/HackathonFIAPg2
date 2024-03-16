3b6ff70fa394c2ef7b44cec5f467a542fae59285
murakamironaldo <murakami@odoias.com.br>
2024-03-16 12:41:53 -0300

    desenvolvimento

---
+++ Hackaton.Fiap.Grupo02.Application/ViewModels/VideoImageInsertView.cs
@@ -0,0 +1,24 @@
+﻿using Hackaton.Fiap.Grupo02.Domain.Entities;
+
+namespace Hackaton.Fiap.Grupo02.Application.ViewModels
+{
+    public class VideoImageInsertView
+    {
+        public string VideoName { get; set; }
+        public string VideoLink { get; set; }
+        public string ZipFile { get; set; }
+        public string ZipName { get; set; }
+
+        public static explicit operator VideoImageInsertView(VideoImage obj)
+        {
+            var model = new VideoImageInsertView
+            {
+                VideoName = obj.VideoName,
+                VideoLink = obj.VideoLink,
+                ZipFile = obj.ZipFile,
+                ZipName = obj.ZipName
+            };
+            return model;
+        }
+    }
+}
